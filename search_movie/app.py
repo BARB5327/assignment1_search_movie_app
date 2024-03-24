dcfef34b7b200295e255765d666cc03b793232f2
BARB5327 <alessandro.barbieri.dias@gmail.com>
2024-03-24 15:20:43 +0100

    update the app

--- search_movie/app.py
+++ search_movie/app.py
@@ -10,7 +10,9 @@ import io
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "JSON_KEY.json"
 client = bigquery.Client(project="cloud-advanced-analytics-1")
 
-# python function and big query functions
+#IN THIS SECTION THERE IS ALL THE PYTHON FUNCTIONS AND BIG QUERY FUNCTIONS
+
+#function to get the autocomplete on the research toolbar 
 def get_autocomplete_titles(user_input):
     query = f"""
     SELECT title, tmdbId FROM `cloud-advanced-analytics-1.assignment1.movies`
@@ -22,6 +24,7 @@ def get_autocomplete_titles(user_input):
     results = query_job.result()
     return [(row.title, row.tmdbId) for row in results]
 
+#Calls the google cloud function to get the information of the film 
 def get_movie_details_from_cloud_function(movie_id):
     cloud_function_url = "https://europe-west6-cloud-advanced-analytics-1.cloudfunctions.net/movie_details"
     params = {"movie_id": str(movie_id)}
@@ -33,12 +36,13 @@ def get_movie_details_from_cloud_function(movie_id):
         st.error(f"Error retrieving film details: {e}")
         return None
 
+# Here ther are different functions for querying the database and retrieving films based on the language, genres
 def get_movies_by_language(language):
     query = f"""
     SELECT title, tmdbId FROM `cloud-advanced-analytics-1.assignment1.movies`
     WHERE LOWER(language) = LOWER('{language}')
     ORDER BY title ASC
-    LIMIT 5
+    LIMIT 25
     """
     query_job = client.query(query)
     results = query_job.result()
@@ -49,12 +53,13 @@ def get_movies_by_genres(genre):
     SELECT title, tmdbId FROM `cloud-advanced-analytics-1.assignment1.movies`
     WHERE LOWER(genres) = LOWER('{genre}')
     ORDER BY title ASC
-    LIMIT 5
+    LIMIT 25
     """
     query_job = client.query(query)
     results = query_job.result()
     return [(row.title, row.tmdbId) for row in results]
 
+#This is used to add the different language, genres and rating available for the differents filters
 def get_available_languages():
     query = """
     SELECT DISTINCT language 
@@ -68,28 +73,7 @@ def get_available_languages():
     except Exception as e:
         st.error(f"Error retrieving languages : {e}")
         return []  
-
-def get_filtered_movies(language, genre, release_year_after, min_average_rating):
-    language_filter = "" if language == "See all languages" else f"AND LOWER(m.language) = LOWER('{language}')"
     
-    genre_filter = "" if genre == "See all genres" else f"AND LOWER(m.genres) LIKE LOWER('%{genre}%')"
-    
-    query = f"""
-    SELECT m.title, m.tmdbId, AVG(r.rating) AS average_rating
-    FROM `cloud-advanced-analytics-1.assignment1.movies` m
-    LEFT JOIN `cloud-advanced-analytics-1.assignment1.ratings` r ON m.movieId = r.movieId
-    WHERE m.release_year > {release_year_after}
-    {language_filter}
-    {genre_filter}
-    GROUP BY m.title, m.tmdbId
-    HAVING AVG(r.rating) >= {min_average_rating}
-    ORDER BY average_rating DESC, m.title ASC
-    LIMIT 5
-    """
-    query_job = client.query(query)
-    results = query_job.result()
-    return [(row.title, row.tmdbId, row.average_rating) for row in results]
-
 
 def get_available_genres():
     query = """
@@ -112,6 +96,30 @@ def get_available_rating():
     return [row.rating for row in results]
 
 
+#Function who display the movies in the advanced research
+def get_filtered_movies(language, genre, release_year_after, min_average_rating):
+    language_filter = "" if language == "See all languages" else f"AND LOWER(m.language) = LOWER('{language}')"
+    
+    genre_filter = "" if genre == "See all genres" else f"AND LOWER(m.genres) LIKE LOWER('%{genre}%')"
+    
+    query = f"""
+    SELECT m.title, m.tmdbId, AVG(r.rating) AS average_rating
+    FROM `cloud-advanced-analytics-1.assignment1.movies` m
+    LEFT JOIN `cloud-advanced-analytics-1.assignment1.ratings` r ON m.movieId = r.movieId
+    WHERE m.release_year > {release_year_after}
+    {language_filter}
+    {genre_filter}
+    GROUP BY m.title, m.tmdbId
+    HAVING AVG(r.rating) >= {min_average_rating}
+    ORDER BY average_rating DESC, m.title ASC
+    LIMIT 25
+    """
+    query_job = client.query(query)
+    results = query_job.result()
+    return [(row.title, row.tmdbId, row.average_rating) for row in results]
+
+
+
 #Streamlit design 
 
 st.title("Search movie ")
@@ -119,13 +127,17 @@ st.title("Search movie ")
     
 st.title(f"***Simple research***")
 
-
+#UI for the simple research section (research toolbar)
 if 'user_input' not in st.session_state or 'selected_movie_id' not in st.session_state:
     st.session_state['user_input'] = ''
     st.session_state['selected_movie_id'] = None
     st.session_state['selected'] = False
 
 user_input = st.text_input("Enter a movie name", value=st.session_state['user_input'])
+
+if user_input=='':
+    st.session_state['selected_movie_id'] = None
+    st.session_state['selected'] = False
 
 if user_input and not st.session_state['selected']:
     suggestions = get_autocomplete_titles(user_input)
@@ -160,13 +172,13 @@ if 'selected_movie_id' in st.session_state and st.session_state['selected_movie_
             trailers = movie_details.get('trailers', [])
             if trailers:
                 st.write("**Trailer :**")
-                for trailer_url in trailers:
-                    st.video(trailer_url)
+                st.video(trailers[0])
             else:
                 st.write("**Trailer :** Trailer not available for the moment")
 
 st.markdown('<hr style="border:2px solid gray; margin-bottom: 20px;"/>', unsafe_allow_html=True)
 
+#UI for the advanced section (differents filter)
 st.title(f"***Advanced research***")
 
 languages = ["See all languages"] + get_available_languages()  
@@ -205,8 +217,7 @@ if st.button('Display movies'):
                         trailers = movie_details.get('trailers', [])
                         if trailers:
                             st.write("**Trailer :**")
-                            for trailer_url in trailers:
-                                st.video(trailer_url)
+                            st.video(trailers[0])
                         else:
                             st.write("Trailer not available for the moment")
                 else:
@@ -214,22 +225,3 @@ if st.button('Display movies'):
     else:
         st.error("No film corresponding to the criteria.")
 
-#background 
-image_path = 'background.jpg'  
-image = Image.open(image_path)
-
-buffered = io.BytesIO()
-image.save(buffered, format="PNG")
-img_data = buffered.getvalue()
-base64_img_data = base64.b64encode(img_data).decode()
-
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] {{
-background-image: url("data:image/jpeg;base64,{base64_img_data}");
-background-size: cover;
-}}
-
-</style>
-"""
-st.markdown(page_bg_img, unsafe_allow_html=True) 
